fba6fa8c29e1cd5c65db160b67217e22653dd351
Pablo Moreira <pablo@pablo-pc.frcon>
2021-12-15 10:51:28 -0300

    Changes to be committed: 	modified:   lib/ds18b20/ds18b20.cpp 	modified:   lib/ds18b20/ds18b20.hpp  Changes not staged for commit: 	modified:   src/main.cpp

--- lib/ds18b20/ds18b20.cpp
+++ lib/ds18b20/ds18b20.cpp
@@ -1,10 +1,24 @@
 #include "ds18b20.hpp"
-#include <OneWire.h>
 
 Ds18b20::Ds18b20(uint8_t pin)
 {
   _pin = pin;
-  OneWire ds(pin);
-
+  OneWire _ds(pin);
+  _num = 0;
+  _index = 0;
   //pinMode(pin, OUTPUT);
+}
+void Ds18b20::search(){
+    if(_ds.search(_addr)){
+      _num++;
+    }
+    else{
+        _ds.reset_search();
+        _num = 0;
+        _index = 0;
+    }
+}
+
+uint8_t Ds18b20::getNum(){
+  return _num;
 }
--- lib/ds18b20/ds18b20.hpp
+++ lib/ds18b20/ds18b20.hpp
@@ -1,18 +1,23 @@
 #ifndef ds18b20_h
 #define ds18b20_h
-
-#include "Arduino.h"
+#include <OneWire.h>
+#include <Arduino.h>
 
 class Ds18b20
 {
     public:
         Ds18b20(uint8_t pin);
-    
+        void search();
+        uint8_t getNum();
     private:
         int _pin;
         boolean _state;
         uint8 _num;
         uint8 _index;
+        byte _data[12];
+        byte _addr[8];
+        OneWire _ds;
+
 };
 
 #endif
